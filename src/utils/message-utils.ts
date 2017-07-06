c97ab60cd1cc208b2dddf0ec9700ceec1e22b2c0
Julien Chakra <julien.chakra@futurice.com>
2017-07-06 10:52:40 +0300

    Match args with space between quotes

--- src/utils/message-utils.ts
+++ src/utils/message-utils.ts
@@ -9,10 +9,25 @@ export interface ParsedMessage {
 export const isHumanMessage = (message: Message, client: Client) => message.author.id !== client.user.id;
 
 export const parseMessage = (message: Message): ParsedMessage => {
-  const splitted = message.content.split(' ');
+
+  const commandRegex = /^(\!\w+) (\w+)((?:"[^"]+"|[^"]+)+)?/g;
+  const matched = commandRegex.exec(message.content);
+
+  const argsRegex = /[ ]+(?=(?:[^"]*"[^"]*")*[^"]*$)/;
+  const argsMatched = (matched[3]) ?
+    matched[3]
+      .split(argsRegex)
+      .filter(s => s && s !== '')
+      .map(s => {
+        if (/"[^"]*"/.test(s)) {
+          return s.slice(1, -1).trim();
+        }
+        return s;
+      }) : [];
+
   return {
-    mainCommand: splitted[0],
-    auxCommand: splitted[1],
-    args: splitted.slice(2)
+    mainCommand: matched[1],
+    auxCommand: matched[2],
+    args: argsMatched
   };
 };
