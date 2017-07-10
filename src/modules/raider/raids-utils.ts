45f229ef7c607376b307661d0fc1f319f6bfe33a
Julien Chakra <julien.chakra@futurice.com>
2017-07-10 18:24:54 +0300

    Add description field in raid display

--- src/modules/raider/raids-utils.ts
+++ src/modules/raider/raids-utils.ts
@@ -4,9 +4,11 @@ import { Raid } from './raids';
 
 export const formatRaidToDisplay = (raid: Raid): string => (
 `\n\`\`\`css
-[${moment(raid.date).format('DD MMM, HH:mm')}] ${raid.name} (ID ${raid.id})\norganizer: ${raid.organizer};
+[${moment(raid.date).format('DD MMM, HH:mm')}] ${raid.name} (ID ${raid.id})
+Organizer: ${raid.organizer};
+Description: ${raid.description};
 
-Participants: ${(raid.players.length > 0) ? raid.players.map(p => p.name).join(',') : '-'}
+Participants: ${(raid.players.length > 0) ? raid.players.map(p => p.name).join(', ') : '-'}
 Waitings: ${(raid.waitings.length > 0) ? raid.waitings.map(p => p.name).join(',') : '-'}
 Absents: ${(raid.absents.length > 0) ? raid.absents.map(p => p.name).join(',') : '-'}
 \`\`\``
